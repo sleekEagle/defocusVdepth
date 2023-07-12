bcb0e7647cf54bd53acbdb28e92943d9d190b52a
sleekEagle <lahirunuwanisme@gmail.com>
2023-07-12 19:28:29 -0400

    a new task script

--- task_def.sh
+++ task_def.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="depvdef"
-#SBATCH --error="/p/blurdepth/models/defnet/def.err"
-#SBATCH --output="/p/blurdepth/models/defnet/def.out"
+#SBATCH --error="def.err"
+#SBATCH --output="def.out"
 #SBATCH --partition="gpu"
-#SBATCH -w ristretto01
+#SBATCH -w jaguar06
 #export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
 #export TORCH_DISTRIBUTED_DEBUG=INFO
-python train_def.py --max_lr 1e-4 --batch_size 4 --rgb_dir refocused_f_75_fdist_2 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/
+python train_def.py --max_lr 1e-4 --batch_size 12 --rgb_dir refocused_f_25_fdist_1 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/
