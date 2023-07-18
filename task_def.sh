1015cf7a954a360f320d6a09b39796f0032d7779
sleekEagle <lahirunuwanisme@gmail.com>
2023-07-18 14:26:15 -0400

    adding task_def.sh

--- task_def.sh
+++ task_def.sh
@@ -3,7 +3,7 @@
 #SBATCH --error="def.err"
 #SBATCH --output="def.out"
 #SBATCH --partition="gpu"
-#SBATCH -w jaguar04
+#SBATCH -w jaguar06
 #export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
 #export TORCH_DISTRIBUTED_DEBUG=INFO
