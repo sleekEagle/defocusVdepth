70acdff736faca315f31393dd7ff8609619e218b
sleekEagle <lahirunuwanisme@gmail.com>
2023-07-20 14:51:40 -0400

    little change

--- train_def.py
+++ train_def.py
@@ -184,7 +184,7 @@ def get_lr(optimizer):
 #iterate though dataset
 print('train_loader len='+str(len(train_loader)))
 logging.info('train_loader len=%s',str(len(train_loader)))
-evalitr=1
+evalitr=10
 best_loss=0
 for i in range(600):
     total_d_loss,total_b_loss=0,0
