63f91146e82d503410279dc5db594e05203b259b
sleekEagle <lahirunuwanisme@gmail.com>
2023-07-21 14:59:46 -0400

    changed from server

--- task_def.sh
+++ task_def.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name="depvdef"
-#SBATCH --error="def.err"
-#SBATCH --output="def.out"
+#SBATCH --job-name="midas"
+#SBATCH --error="midas.err"
+#SBATCH --output="midas.out"
 #SBATCH --partition="gpu"
 #SBATCH -w jaguar06
 #export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
 #export TORCH_DISTRIBUTED_DEBUG=INFO
-python train_def.py --max_lr 1e-4 --batch_size 12 --rgb_dir refocused_f_25_fdist_2 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/
+python train_def.py --max_lr 1e-4 --batch_size 12 --rgb_dir refocused_f_25_fdist_2 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/ --model_name midas
--- task_def3.sh
+++ task_def3.sh
@@ -7,4 +7,4 @@
 #export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
 #export TORCH_DISTRIBUTED_DEBUG=INFO
-python train_def.py --max_lr 1e-4 --batch_size 12 --rgb_dir refocused_f_25_fdist_3 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/
+python train_def.py --max_lr 1e-4 --batch_size 12 --rgb_dir refocused_f_25_fdist_5 --data_path /p/blurdepth/data/ --depth_dir rawDepth --resultspth /p/blurdepth/models/defnet/
